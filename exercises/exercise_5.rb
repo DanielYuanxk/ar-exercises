d2d14e7545e93925fdb936d8d1f682f90cdd975d
DanielYuanxk <dyxk1234@hotmail.com>
2023-03-07 16:12:30 -0500

    finish exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,15 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+total_revenue = Store.sum('annual_revenue')
+puts "Total revenue: $#{total_revenue}"
+
+average_revenue = Store.average('annual_revenue')
+puts "Average annual revenue: $#{average_revenue}"
+
+million_dollar_stores = Store.where('annual_revenue >= ?', 1_000_000).count
+puts "#{million_dollar_stores} "
